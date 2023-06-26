35503226abea316987245535bde489e2f19ae8fc
AbdullahAshikAdnan <57279317+AbdullahAshikAdnan@users.noreply.github.com>
2023-06-26 22:01:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ async function sendRVM(phoneNumber, voicemailFile) {
 }
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(10000, () => {
+  console.log("Server is running on port 10000");
 });
