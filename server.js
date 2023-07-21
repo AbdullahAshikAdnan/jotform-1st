6e6fcd3ccc090e4a23357e11a7b88cf93d4a67a3
AbdullahAshikAdnan <57279317+AbdullahAshikAdnan@users.noreply.github.com>
2023-07-21 21:29:12 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,67 +1,79 @@
 const express = require('express');
 const app = express();
-
-// Import required modules and dependencies
 const multer = require('multer');
-const axios = require('axios');
-const FormData = require('form-data');
+const upload = multer({ dest: 'uploads/' });
 const fs = require('fs');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const FormData = require('form-data');
 
-// Configure multer for file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Specify the directory where you want to store the uploaded files
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+// Configure middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configure JotForm API credentials
+const jotformApiKey = "27f50030f5db987ecbf9f985f47076ec";
+const jotformFormId = "231365209409051";
+const jotformApiUrl = "https://api.jotform.com";
+
+// Configure Drop Cowboy API credentials
+const dropCowboyTeamId = "acb1088b-e24d-4c61-bb83-cfb335d71892";
+const dropCowboySecretKey = "48db8265-f632-418b-b4f3-eb8c24aaef58";
+const dropCowboyApiUrl = "https://api.dropcowboy.com/v1/rvm";
+
+// Define route for the root URL
+app.get("/", (req, res) => {
+  res.send("Hello, Glitch!");
 });
-
-const upload = multer({ storage });
-
-// Define the route for the JotForm webhook
-app.post('/jotform-webhook', upload.single('voicemail'), async (req, res) => {
+// Define route for JotForm webhook
+app.post("/jotform-webhook", async (req, res) => {
   // Extract form data from JotForm submission
-  const phoneNumber = req.body.input_5_phone;
-  const voicemailFile = req.file;
+  const phoneNumber = req.body.input_3;
+  const recordingId = "4a57733e-30ab-43da-96a6-75e981e41fa9";
 
   // Perform any necessary validation on the form data
 
   // Call function to send RVM
   try {
-    await sendRVM(phoneNumber, voicemailFile);
-    res.status(200).json({ message: 'RVM sent successfully' });
+    await sendRVM(phoneNumber, recordingId);
+    res.status(200).json({ message: "RVM sent successfully" });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to send RVM' });
+    console.error("Error sending RVM:", error.message);
+    res.status(500).json({ error: "Failed to send RVM" });
   }
 });
 
 // Function to send RVM
-async function sendRVM(phoneNumber, voicemailFile) {
+async function sendRVM(phoneNumber, recordingId) {
   // Use Axios to make API request to send RVM
-  const endpoint = 'https://api.dropcowboy.com/v1/rvm'; // Replace with the actual API endpoint
-  const url = endpoint;
+  const url = dropCowboyApiUrl;
 
   // Set request headers and parameters
   const headers = {
-    'X-TeamID': 'acb1088b-e24d-4c61-bb83-cfb335d71892',
-    'X-SecretKey': '48db8265-f632-418b-b4f3-eb8c24aaef58',
-    'Content-Type': 'multipart/form-data',
+    "X-TeamID": dropCowboyTeamId,
+    "X-SecretKey": dropCowboySecretKey,
+    "Content-Type": "multipart/form-data",
   };
 
   const formData = new FormData();
-  formData.append('phone_number', phoneNumber);
-  formData.append('voicemail', fs.createReadStream(voicemailFile.path));
-
+  formData.append("phone_number", phoneNumber);
+  formData.append("recording_id", recordingId); 
+  
   // Make the API request
-  const response = await axios.post(url, formData, { headers });
+  const response = await axios.post(url, formData, {
+    headers: formData.getHeaders(), // Use getHeaders() to retrieve the correct headers for FormData
+    onUploadProgress: (progressEvent) => {
+      const progress = (progressEvent.loaded / progressEvent.total) * 100;
+      console.log(`Upload progress: ${progress.toFixed(2)}%`);
+    },
+  });
 
   // Handle the response as needed
-  console.log('RVM sent:', response.data);
+  console.log("RVM sent:", response.data);
 }
 
 // Start the server
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = 10000; // You can change the port here if needed
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
